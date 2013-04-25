5a29c28f3feba4dcd55c37e419508d8c48dbfc0a
Michael Williamson <mike@zwobble.org>
2013-04-25 14:55:30 +0100

    Allow different examples to be loaded

--- demo.js
+++ demo.js
@@ -1,23 +1,44 @@
-glimpse.data.initMetadata({});
-glimpse.data.initData({
-    method:  'Get',
-    browser: '',
-    clientId: 'Chrome 22.0',
-    dateTime: '',
-    requestId: '1234' + (window.location.search ? Math.round(new Date().getTime() / 1000) : ''),
-    isAjax: false,
-    uri: location.href,
-    data: {
-        "Request": {
-            name: 'Request',
-            data: {
-                url: location.href,
-                protocol: location.protocol,
-                host: location.host,
-                pathname: location.pathname,
-                search: location.search,
-                hash: location.hash
+(function($) {
+    var queryString = readQueryString();
+    
+    var examples = [
+        "minimal"
+    ];
+    
+    var exampleName = queryString.example || examples[0];
+    var exampleUrl = "examples/" + exampleName + ".js";
+    
+    $.ajax({
+        url: exampleUrl,
+        dataType: "text",
+        success: function(source) {
+            loadScript(exampleUrl);
+            $("#source-raw-link").attr("href", exampleUrl);
+            $("#source").text(source);
+        }
+    });
+    
+    
+    function loadScript(src) {
+        var scriptTag = document.createElement("script");
+        scriptTag.type = "text/javascript";
+        scriptTag.async = true;
+        scriptTag.src = src;
+        document.body.appendChild(scriptTag);
+    };
+    
+    function readQueryString() {
+        var pairs = window.location.search.substr(1).split('&');
+        if (queryString == "") {
+            return {};
+        }
+        var result = {};
+        for (var i = 0; i < pairs.length; ++i) {
+            var keyValue = pairs[i].split('=');
+            if (keyValue.length == 2) {
+                result[keyValue[0]] = decodeURIComponent(keyValue[1].replace(/\+/g, " "));
             }
         }
-    }
-});
+        return result;
+    };
+})(jQueryGlimpse);
